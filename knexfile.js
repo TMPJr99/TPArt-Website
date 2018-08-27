52dd6ddc8e6419bf3c86a1f5477e6f95c90c1f41
Todd Porter <toddtorter@Todds-MacBook-Air.local>
2018-08-27 11:45:12 -0700

    hvcbc

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
       client: 'pg',
       connection: {
-        database: "TPArt-Web",
+        database: "TPArt-Website",
         host: "localhost"
       },
       migrations: {
