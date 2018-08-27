0535b6e268e79fb97ca75fd36a2eaa06e9e5b460
Todd Porter <toddtorter@Todds-MacBook-Air.local>
2018-08-27 15:11:39 -0700

    damn this carousel

--- controllers/painting.js
+++ controllers/painting.js
@@ -3,7 +3,9 @@ const knex = require("../db/knex.js");
 module.exports = {
   // CHANGE ME TO AN ACTUAL FUNCTION
   home: function(req, res) {
-    res.render("home");
+      knex('painting').then((result)=>{
+        res.render("home", {paintings:result});
+      })
   },
 
   shop: (req, res)=>{
