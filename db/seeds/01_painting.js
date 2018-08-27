b15371506cd0ef830b62a940ec15d40208c353f1
Todd Porter <toddtorter@Todds-MacBook-Air.local>
2018-08-27 13:31:16 -0700

    seeded painting

---
+++ db/seeds/01_painting.js
@@ -0,0 +1,11 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('painting').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('painting').insert([
+        {genre: 'abstract', series:"none", price:100, img_location:"/images/placeholder.png"}
+      ]);
+    });
+};
