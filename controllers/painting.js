f6800edc8f24aea324071a9cb233fef5babe39f8
Todd Porter <toddtorter@Todds-MacBook-Air.local>
2018-08-28 15:35:09 -0700

    fixed sessions a little

--- controllers/painting.js
+++ controllers/painting.js
@@ -3,31 +3,37 @@ const knex = require("../db/knex.js");
 module.exports = {
   // CHANGE ME TO AN ACTUAL FUNCTION
   home: (req, res) => {
-      knex('painting').then((result)=>{
-        res.render('home', {paintings: result});
+    if (!req.session.cart) {
+      req.session.cart = [];
+    }
+    if (!req.session.wishlist) {
+      req.session.wishlist = [];
+    }
+    if (!req.session.total) {
+      req.session.total = 0;
+    }
+    knex('painting').then((result) => {
+      res.render('home', {
+        paintings: result
+      });
+    })
+  },
+
+  shop: (req, res) => {
+    knex('painting')
+      .then((results) => {
+        res.render('shop', {
+          paintings: results
+        });
       })
   },
 
-  shop: (req, res)=>{
-    if(!req.session.cart){
-      req.session.cart = [];
-    }
-    if(!req.session.wishlist){
-      req.session.wishlist = [];
-    }
-    if(!req.session.total){
-      req.session.total = 0;
-    }
-    knex('painting')
-    .then((results)=>{
-      res.render('shop', {paintings: results});
-    })
-  },
-
-  canvas: (req, res)=>{
+  canvas: (req, res) => {
     knex('painting').where('series', 'canvas')
-    .then((results)=>{
-      res.render('tp_on_canvas', {canvas: results});
-    })
+      .then((results) => {
+        res.render('tp_on_canvas', {
+          canvas: results
+        });
+      })
   },
 }
