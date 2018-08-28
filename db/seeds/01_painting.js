a6062048b0b325fc5589e4269a6eb7702a1a6fb8
Todd Porter <toddtorter@Todds-MacBook-Air.local>
2018-08-28 10:08:34 -0700

    fixed seed

--- db/seeds/01_painting.js
+++ db/seeds/01_painting.js
@@ -5,11 +5,11 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('painting').insert([
-        {genre: 'abstract', series:"none", tier:2, img_location:"public/images/Aurora_low res.jpg"},
-        {genre: 'abstract', series:"canvas", tier:1, img_location:"public/images/Far Far Away_low res.jpg"},
-        {genre: 'abstract', series:"canvas", tier:2, img_location:"public/images/Flower_low res.jpg"},
-        {genre: 'abstract', series:"canvas", tier:1, img_location:"public/images/Abstract Knife unnamed_low res.jpg"},
-        {genre: 'abstract', series:"canvas", tier:2, img_location:"public/images/Mothers Tree 8x10_low res.jpg"},
+        {genre: 'abstract', series:"none", tier:2, img_location:"/images/Aurora_low res.jpg"},
+        {genre: 'abstract', series:"canvas", tier:1, img_location:"/images/Far Far Away_low res.jpg"},
+        {genre: 'abstract', series:"canvas", tier:2, img_location:"/images/Flower_low res.jpg"},
+        {genre: 'abstract', series:"canvas", tier:1, img_location:"/images/Abstract Knife unnamed_low res.jpg"},
+        {genre: 'abstract', series:"canvas", tier:2, img_location:"/images/Mothers Tree 8x10_low res.jpg"},
       ]);
     });
 };
