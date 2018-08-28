367b8b1f5eefef8bb5a35174dd103bbd577fd4fe
Todd Porter <toddtorter@Todds-MacBook-Air.local>
2018-08-28 10:05:18 -0700

    chaned db

--- db/migrations/20180827115502_painting.js
+++ db/migrations/20180827115502_painting.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
     table.increments();
     table.string('genre');
     table.string('series');
-    table.integer('price');
+    table.integer('tier');
     table.string('img_location');
     table.timestamps(true, true);
   })
