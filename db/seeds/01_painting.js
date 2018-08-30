28b547f449b513b5617fb5b5049d1dc13c23646c
Todd Porter <toddtorter@Todds-MacBook-Air.local>
2018-08-30 13:29:09 -0700

    fixed carousel and changed painting data

--- db/seeds/01_painting.js
+++ db/seeds/01_painting.js
@@ -14,8 +14,8 @@ exports.seed = function(knex, Promise) {
         {genre: 'abstract', series:"canvas", size:"8x10", tier:1, img_location:"images/Far Far Away_low res.jpg"},
         {genre: 'abstract', series:"none", tier:2, img_location:"images/Flower_low res.jpg"},
         {genre: 'impressionist', series:"none", tier:1, img_location:"images/Mothers Tree 8x10_low res.jpg"},
-        {genre: 'abstract', series:"canvas", tier:2, size:"16x20", img_location:"images/Resolute_low res.jpg"},
-        {genre: 'abstract', series:"none", tier:2, img_location:"images/Fire Island_low res.jpg"},
+        {genre: 'impressionist', series:"canvas", tier:2, size:"16x20", img_location:"images/Resolute_low res.jpg"},
+        {genre: 'landscape', series:"none", tier:2, img_location:"images/Fire Island_low res.jpg"},
         {genre: 'landscape', series:"none", tier:1, img_location:"images/Page Springs_8x10_LOW res.jpg"},
       ]);
     });
